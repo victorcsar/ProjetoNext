53aebca998f09e481ec3adca484971a1012dbf3c
victorcsar <victorcesagx@gmail.com>
2024-02-28 12:43:42 -0300

    aula 23

--- src/pages/inputs/inputs.tsx
+++ src/pages/inputs/inputs.tsx
@@ -1,6 +1,8 @@
 import Topo from "@/components/Topo";
 import { useState } from "react";
 
+const disponiveis = ["sim", "nao"]
+
 
 export default function inputs() {
     const [produto, setNomeProduto] = useState<string>("test")
@@ -12,18 +14,20 @@ export default function inputs() {
             <Topo></Topo>
             <div className="campoForm">
                 <label htmlFor="">produto</label>
-                <input type="text" value={produto} onChange={(evt)=>setNomeProduto(evt.target.value)} />
+                <input type="text" value={produto} onChange={(evt) => setNomeProduto(evt.target.value)} />
             </div>
             <div className="campoForm">
                 <label htmlFor="">valor</label>
-                <input type="text" value={valor} onChange={(evt)=>setValor(evt.target.value)}/>
+                <input type="text" value={valor} onChange={(evt) => setValor(evt.target.value)} />
             </div>
             <div className="campoForm">
                 <label htmlFor="">Diponivel:</label>
-                <select value={disponivel} onChange={(evt)=>setDisponivel(evt.target.value)}>
-                   <option value=""></option> 
-                   <option value="Nao">Não</option> 
-                   <option value="Sim">Sim</option> 
+                <select value={disponivel} onChange={(evt) => setDisponivel(evt.target.value)}>
+                    {
+                        disponiveis.map((d:any)=>{
+                            return <option value={d}>{d}</option>
+                        })
+                    }
                 </select>
             </div>
             <div>Produto digitado: {produto}</div>
